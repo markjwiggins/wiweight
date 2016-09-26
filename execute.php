f514ed6db8e78be625744420d13b5690d5347bbf
markjwiggins <markjwiggins@gmail.com>
2016-09-26 20:54:08 +0100

    Sorted

--- execute.php
+++ execute.php
@@ -1,4 +1,6 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . '/home/wipi/www/sandbox/wiWeight/google-api-php-client/src');
+
 $output = $_POST;
 
 //echo json_encode($output);
@@ -11,10 +13,16 @@ $data['age'] = 40;
 $newJsonString = json_encode($data);
 $file = file_put_contents('data.json', $newJsonString);
 
-if (empty($file)) {
-  echo 'failed...';
+if (is_writable('data.json')) {
+  echo 'Yep, I can write!';
 } else {
-  echo 'success...';
+  echo 'Fraid Not!';
 }
+
+// if (empty($file)) {
+//   echo 'failed...';
+// } else {
+//   echo 'success...';
+// }
 
 //echo json_encode($data);
